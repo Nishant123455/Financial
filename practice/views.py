033581f5e302fb7366c58d975cc227016174268f
Nishant123455 <nishant.219302089@muj.manipal.edu>
2022-06-20 16:21:08 +0530

    sample commit

---
+++ practice/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.http  import HttpResponse
+# Create your views here.
+def  home(request):
+    return render(request, 'FA.html')
